d308e72e2a622f750b5d7a195531e2470913ead6
Dineshwar Doddapaneni <63300423+dinesh9-ai@users.noreply.github.com>
2022-01-23 21:58:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+
+from flask import Flask, render_template,request,redirect,url_for,flash,send_from_directory
+from PIL import Image, ImageFont, ImageDraw 
+import os
+
+# app = Flask(__name__) # to make the app run without any
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/success/<name>')
+def success(name):
+    if name=='not':
+        return 'enter correct Roll number or In case if you participated and still not getting certificate contact the coordinators'
+
+
+
+@app.route("/pyt",methods=['GET','POST'])
+def pyt():
+    if request.method == 'POST':
+        user = request.form['nm']
+        if user=='' or user==None:
+            user='not'
+            return redirect(url_for('success', name = user))
+        user=user+'.pdf'
+        if user not in os.listdir('./static/styles/im'):
+            user='not'
+            return redirect(url_for('success', name = user))
+        else:
+            return send_from_directory(directory='static', filename='styles/im/' +user) 
+    else:
+        return render_template('pyt.html')
+
+      
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+
+@app.route("/tasks")
+def tasks():
+    return render_template('syllabus.html')
+@app.route("/events")
+def events():
+    return render_template('event.html')
+
+
+@app.route("/faq")
+def faq():
+    return render_template('faq.html')
